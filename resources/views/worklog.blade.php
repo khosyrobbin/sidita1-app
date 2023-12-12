bf73312db262ba53b446a35bd568b52baad15360
khosyrobbin <khosybin@gmail.com>
2023-12-12 20:22:51 +0700

    update nav active

--- resources/views/worklog.blade.php
+++ resources/views/worklog.blade.php
@@ -57,6 +57,7 @@
                         <div class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
                             <select name="hours_worked" id="hours_worked"
                                 class="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input">
+                                <option value="">Pilih Jumlah Jam Kerja</option>
                                 <option value="1">1 Jam</option>
                                 <option value="2">2 Jam</option>
                                 <option value="3">3 Jam</option>
@@ -106,53 +107,55 @@
                         <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
                             @foreach ($worklog as $w)
                                 <?php $project = App\Models\ProjectModel::find($w->project_id); ?>
-                                <tr class="text-gray-700 dark:text-gray-400">
-                                    <td class="px-4 py-3 text-sm">
-                                        {{ $loop->iteration }}
-                                    </td>
-                                    <td class="px-4 py-3 text-xs">
-                                        {{ Auth::user()->name }}
-                                    </td>
-                                    <td class="px-4 py-3 text-xs">
-                                        {{ $project->nama_project }}
-                                    </td>
-                                    <td class="px-4 py-3 text-sm">
-                                        {{ $w->work_date }}
-                                    </td>
-                                    <td class="px-4 py-3 text-sm">
-                                        {{ $w->hours_worked }} Jam
-                                    </td>
-                                    <form action="{{ route('worklog.destroy', $w->id) }}" method="post">
-                                        @csrf
-                                        <td class="px-4 py-3">
-                                            <div class="flex items-center space-x-4 text-sm">
-                                                <a href="{{ route('worklog.edit', $w->id) }}"
-                                                    class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                                                    aria-label="Edit">
-                                                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
-                                                        viewBox="0 0 20 20">
-                                                        <path
-                                                            d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z">
-                                                        </path>
-                                                    </svg>
-                                                </a>
-
-                                                @method('DELETE')
-                                                <button type="submit"
-                                                    class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                                                    aria-label="Delete">
-                                                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
-                                                        viewBox="0 0 20 20">
-                                                        <path fill-rule="evenodd"
-                                                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                                            clip-rule="evenodd"></path>
-                                                    </svg>
-                                                </button>
-
-                                            </div>
+                                @if ($w->user_id == Auth::user()->id)
+                                    <tr class="text-gray-700 dark:text-gray-400">
+                                        <td class="px-4 py-3 text-sm">
+                                            {{ $loop->iteration }}
                                         </td>
-                                    </form>
-                                </tr>
+                                        <td class="px-4 py-3 text-xs">
+                                            {{ Auth::user()->name }}
+                                        </td>
+                                        <td class="px-4 py-3 text-xs">
+                                            {{ $project->nama_project }}
+                                        </td>
+                                        <td class="px-4 py-3 text-sm">
+                                            {{ $w->work_date }}
+                                        </td>
+                                        <td class="px-4 py-3 text-sm">
+                                            {{ $w->hours_worked }} Jam
+                                        </td>
+                                        <form action="{{ route('worklog.destroy', $w->id) }}" method="post">
+                                            @csrf
+                                            <td class="px-4 py-3">
+                                                <div class="flex items-center space-x-4 text-sm">
+                                                    <a href="{{ route('worklog.edit', $w->id) }}"
+                                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                                        aria-label="Edit">
+                                                        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
+                                                            viewBox="0 0 20 20">
+                                                            <path
+                                                                d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z">
+                                                            </path>
+                                                        </svg>
+                                                    </a>
+
+                                                    @method('DELETE')
+                                                    <button type="submit"
+                                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                                        aria-label="Delete">
+                                                        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
+                                                            viewBox="0 0 20 20">
+                                                            <path fill-rule="evenodd"
+                                                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                                                clip-rule="evenodd"></path>
+                                                        </svg>
+                                                    </button>
+
+                                                </div>
+                                            </td>
+                                        </form>
+                                    </tr>
+                                @endif
                             @endforeach
                         </tbody>
                     </table>
